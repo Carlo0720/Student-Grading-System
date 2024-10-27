6ab4c3b69595a528b1dd49c9344526017a25f05b
Cheeseeeees <almero.120726@fairview.sti.edu.ph>
2024-10-27 20:58:47 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿namespace StudentGradingSystem
+{
+    internal class Program
+    {
+        class Student
+        {
+            public string Name { get; set; }
+            public int ID { get; set; }
+            public string Course { get; set; }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            List<Student> students = new List<Student>();
+
+            
+
+            
+
+            while (true)
+            {
+                DisplayMeu();
+                string response = Console.ReadLine();
+                switch (response)
+                {
+                    case "1":
+                        Console.WriteLine("what");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("what");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("what");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("what");
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Exiting...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            }
+
+
+
+        }
+
+
+
+        static void DisplayMeu()
+        {
+            Console.WriteLine("   ▄████████    ▄██████▄  ████████▄ ");
+            Console.WriteLine("  ███    ███   ███    ███ ███   ▀███ ");
+            Console.WriteLine("  ███    █▀    ███    █▀  ███    ███ ");
+            Console.WriteLine("  ███         ▄███        ███    ███ ");
+            Console.WriteLine("▀███████████ ▀▀███ ████▄  ███    ███ ");
+            Console.WriteLine("        ███   ███     ███ ███    ███ ");
+            Console.WriteLine("   ▄█    ███   ███    ███ ███   ▄███ ");
+            Console.WriteLine("▄████████▀    ████████▀  ████████▀  ");
+             
+            Console.WriteLine("Welcome to the student grading system");
+            Console.WriteLine("1. Add Student");
+            Console.WriteLine("2. Add Course");
+            Console.WriteLine("3. Add Grade");
+            Console.WriteLine("4. View student report");
+            Console.WriteLine("5. Exit  ");
+        }
+    }
+
+   
+
+
+   
+
+}
+
