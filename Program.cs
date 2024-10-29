f6a1c24797baeacaebcb4c764ac70ba5e4444279
Cheeseeeees <almero.120726@fairview.sti.edu.ph>
2024-10-29 17:58:31 +0800

    Added a function to the AddStudent Method

--- Program.cs
+++ Program.cs
@@ -1,33 +1,49 @@
-﻿namespace StudentGradingSystem
+﻿using System.Collections.Generic;
+
+namespace StudentGradingSystem
 {
     internal class Program
     {
         class Student
         {
-            public string Name { get; set; }
             public int ID { get; set; }
-            public string Course { get; set; }
+
+            public string Name { get; set; }
+            
+            public List<String> EnrolledCourses { get; set; } = new List<String>();
+
+
+            static void AddCourse()
+            {
+
+            }
+
+            static void removeCourse()
+            {
+
+            }
+
+            static void CalcAveGrade()
+            {
+
+            }
 
         }
 
 
+        static List<Student> students = new List<Student>();
+      
+
         static void Main(string[] args)
         {
-
-            List<Student> students = new List<Student>();
-
-            
-
-            
-
             while (true)
             {
-                DisplayMeu();
+                DisplayMenu();
                 string response = Console.ReadLine();
                 switch (response)
                 {
                     case "1":
-                        Console.WriteLine("what");
+                        AddNewStudent();
                         break;
 
                     case "2":
@@ -52,36 +68,91 @@
                 }
             }
 
+            static void AddNewStudent()
+            {
+                Student student = new Student();
 
+                student.ID = students.Count + 1;
+                student.Name = GetInput("Please enter Student Name: ");
+
+
+
+                while (true)
+                {
+                    string course = GetInput("Please enter course (or type done if finished): ");
+
+                    if (course.ToLower() == "done")
+                    {
+                        break;
+                    }
+
+                    student.EnrolledCourses.Add(course);
+                }
+
+                students.Add(student);
+
+                Console.WriteLine("Student added successfully ");             
+                Console.WriteLine("Press any key to return to the main menu...");
+                Console.ReadKey();
+
+
+                
+
+            }
+
+
+
+            static string GetInput(string prompt, string defualtval = "")
+            {
+                Console.WriteLine(prompt);
+                string input = Console.ReadLine();
+                return string.IsNullOrEmpty(input) ? defualtval : input;
+            }
+
+            static double GetValidDoubleInput(string propmt, double defaultval = -1)
+            {
+                double val;
+                Console.WriteLine(propmt);
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out val) && val >= 0)
+                {
+                    return val;
+                }
+
+                return defaultval != -1 ? defaultval : GetValidDoubleInput(propmt);
+
+            }
+
+            static void DisplayMenu()
+            {
+                Console.WriteLine("   ▄████████    ▄██████▄  ████████▄ ");
+                Console.WriteLine("  ███    ███   ███    ███ ███   ▀███ ");
+                Console.WriteLine("  ███    █▀    ███    █▀  ███    ███ ");
+                Console.WriteLine("  ███         ▄███        ███    ███ ");
+                Console.WriteLine("▀███████████ ▀▀███ ████▄  ███    ███ ");
+                Console.WriteLine("        ███   ███     ███ ███    ███ ");
+                Console.WriteLine("   ▄█    ███   ███    ███ ███   ▄███ ");
+                Console.WriteLine("▄████████▀    ████████▀  ████████▀  ");
+
+                Console.WriteLine("Welcome to the Student Grading System");
+                Console.WriteLine("1. Add Student");
+                Console.WriteLine("2. Add Course");
+                Console.WriteLine("3. Add Grade");
+                Console.WriteLine("4. View student report");
+                Console.WriteLine("5. Exit  ");
+            }
 
         }
 
 
 
-        static void DisplayMeu()
-        {
-            Console.WriteLine("   ▄████████    ▄██████▄  ████████▄ ");
-            Console.WriteLine("  ███    ███   ███    ███ ███   ▀███ ");
-            Console.WriteLine("  ███    █▀    ███    █▀  ███    ███ ");
-            Console.WriteLine("  ███         ▄███        ███    ███ ");
-            Console.WriteLine("▀███████████ ▀▀███ ████▄  ███    ███ ");
-            Console.WriteLine("        ███   ███     ███ ███    ███ ");
-            Console.WriteLine("   ▄█    ███   ███    ███ ███   ▄███ ");
-            Console.WriteLine("▄████████▀    ████████▀  ████████▀  ");
-             
-            Console.WriteLine("Welcome to the student grading system");
-            Console.WriteLine("1. Add Student");
-            Console.WriteLine("2. Add Course");
-            Console.WriteLine("3. Add Grade");
-            Console.WriteLine("4. View student report");
-            Console.WriteLine("5. Exit  ");
-        }
+
     }
 
-   
 
 
-   
+
+
 
 }
 
